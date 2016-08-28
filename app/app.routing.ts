d46a0b5011a93a364966bb4f6f7957c310a10ff6
jelliotartz <jelliotartz@gmail.com>
2016-08-28 17:22:22 -0500

    Add dashboard component and route.

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -5,11 +5,21 @@ import {
 } from '@angular/router'
 
 import { HeroesComponent } from './heroes.component'
+import { DashboardComponent } from './dashboard.component'
 
 const appRoutes: Routes = [
   {
     path: 'heroes',
     component: HeroesComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
   }
 ]
 
---
+++ app/dashboard.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'my-dashboard',
+  template: `
+    <h3>my dashboard</h3>
+  `
+})
+export class DashboardComponent { }
