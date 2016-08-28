85c46f14e03d3bbb47e5d75273967c5e1cb25531
jelliotartz <jelliotartz@gmail.com>
2016-08-28 15:40:03 -0500

    Separate HeroDetail and Hero components into separate files.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero'
 
 const HEROES: Hero[] = [
   { id: 11, name: 'mr dill' },
@@ -18,7 +15,6 @@ const HEROES: Hero[] = [
   { id: 20, name: 'cayenne' }
 ];
 
-
 @Component({
     selector: 'my-app',
     template:`
@@ -31,15 +27,7 @@ const HEROES: Hero[] = [
           <span class='badge'>{{hero.id}}</span> {{hero.name}}
         </li>
       </ul>
-
-      <div *ngIf="selectedHero">selected spice hero:
-        <h2>{{selectedHero.name}} details!</h2>
-        <div><label>id:</label>{{selectedHero.id}}</div>
-        <div>
-          <label>name: </label>
-          <input [(ngModel)]="selectedHero.name" placeholder='name'/>
-        </div>
-      </div>
+      <my-hero-detail [hero]="selectedHero"></my-hero-detail>
    `,
    styles: [`
     .selected {
