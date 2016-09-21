4458f5b3a83d23e4bb11e5d0e96962079f3c19ef
jelliotartz <jelliotartz@gmail.com>
2016-09-20 22:11:31 -0500

    Add delete functionality for spice heroes.

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,3 +1,5 @@
+//tslint:disable
+
 import {
   Component,
   OnInit,
@@ -45,6 +47,17 @@ export class HeroesComponent implements OnInit {
         this.heroes.push(hero)
         this.selectedHero = null;
       });
-}
+  }
+
+  delete(hero: Hero): void {
+    this.heroService.delete(hero.id).then(
+      () => {
+        this.heroes = this.heroes.filter(h => h !== hero)
+        if (this.selectedHero === hero) {
+          this.selectedHero = null
+        }
+      }
+    )
+  }
 
 }
